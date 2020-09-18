1579fb2690a0895134cc44f5cf52586238f82c22
Alyssa Easterly <alyssa.easterly@yahoo.com>
2020-09-18 18:29:17 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,6 +1,6 @@
 def find_even_values(src)
   row_index = 0 
-  while row_index < sr.count do
+  while row_index < src.count do
     element_index = 0 
     while element_index < src[rox_index].count do
       if src[row_index][element_index].even?
@@ -9,4 +9,4 @@ def find_even_values(src)
     end
     row_index += 1 
   end
-end
+end 
